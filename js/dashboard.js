43fbb255ab89bdc934bb6dbc7783c1e63066212d
Erik Bautista Santibanez <ebautist@stedwards.edu>
2021-04-25 00:30:52 +0000

    Merged in dashboard-improvements (pull request #13)
    Dashboard improvements
    
    * Added navigation bar
    
    * Fix issues importing common services files

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,38 +1,74 @@
-$(() => {
-    let cloud = window.location.hostname.split('.')[0]
-    let cloudURL = `https://${cloud}.team22.sweispring21.tk`
+/* Global Variables */
+let cloud = window.location.hostname.split('.')[0]
+let cloudURL = `https://${cloud}.team22.sweispring21.tk`
 
-    // Handle user data if there is someone is logged in, else redirect them to login page
+/* Adds shadow on header once it passes height */
+$(window).scroll(() => {
+    let header = $("header")
+    if (window.scrollY > 0) {
+        header.addClass("header-scroll");
+    } else {
+        header.removeClass("header-scroll");
+    }
+});
 
-    // fetchLoggedInUser().then(data => {
-    //     if (data.status === 200) {
-    //         // Success in fetching data now update dashboard with the new data
-    //     } else {
-    //         // Was not able to fetch user
-    //         window.location.replace(cloudURL)
-    //     }
-    // })
-
-    // If a user clicks on the logout button, cookies will be empty and will be taken to main page
-    $('#logoutButton').click(() => {
-        let data = {
-            "cloud": cloud
+// If the user resizes and the tab is still shown, we will need to hide it and show the navigation
+$(window).resize(() => {
+    let windowWidth = $(window).width();
+    if (windowWidth > 991) {
+        let navBar = $('#navbar');
+        let hasMobileNavbar = navBar.hasClass('navbar-mobile');
+        if (hasMobileNavbar) {
+            navBar.removeClass('navbar-mobile');
+            let mobileNavToggle = $('#mobile-nav-toggle');
+            mobileNavToggle.removeClass('text-white');
+            mobileNavToggle.addClass('text-secondary');
+            mobileNavToggle.removeClass('bi-x');
+            mobileNavToggle.addClass('bi-list');
         }
-        fetch(cloudURL + "/api/v1/common-services/logout", {
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return Promise.reject(response);
-        }).then(data => {
-            window.location.replace(cloudURL);
-        }).catch(error => {
-            // Handle error
-        })
+    }
+});
+
+// Switch from hamburger icon to x icon if pressed
+$(() => {
+    $('#mobile-nav-toggle').click(function () {
+        let hasList = $(this).hasClass('bi-list');
+        if (hasList) {
+            $(this).removeClass('bi-list');
+            $(this).addClass('bi-x');
+            $(this).removeClass('text-secondary');
+            $(this).addClass('text-white');
+            $('#navbar').addClass('navbar-mobile');
+        } else {
+            $(this).removeClass('bi-x');
+            $(this).addClass('bi-list');
+            $(this).removeClass('text-white');
+            $(this).addClass('text-secondary');
+            $('#navbar').removeClass('navbar-mobile');
+        }
+    });
+})
+
+// General main func once documents finished loading
+$(() => {
+    // This function checks to see if there is credentials saved. If so just direct them to the dashboard
+    fetchLoggedInUser(cloud).then(response => {
+        // Success getting user
+        if (response.status === 200) {
+        } else {
+            // Failed to get user with token
+            // window.location.replace(cloudURL)
+        }
+    }).catch(error => {
+        // Error fetching
+        $("#mainAlert").text("There was an error getting information.")
+        console.error("Error fetching: " + error)
+    })
+})
+
+// Logout button
+$(() => {
+    $("#logoutButton").click(() => {
+        logoutUser();
     });
 });
